9e6e0d97a7da46bc57086ec4d1d5a1a637957192
Brian <h99y5246cv@gmail.com>
2024-01-21 10:22:48 +0800

    Update UI and routing configuration
    Update theme color and background color

--- src/app/features/add-expense/add-expense.component.ts
+++ src/app/features/add-expense/add-expense.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, type OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-expense',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
   ],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.css',
