ce1d40be1bf292d79549a741d8b9a40c5d118916
Brian <h99y5246cv@gmail.com>
2024-01-30 14:25:05 +0800

    Refactor tag list retrieval in TagListGroupResolver

--- src/app/core/resolvers/tag-list-group.resolver.ts
+++ src/app/core/resolvers/tag-list-group.resolver.ts
@@ -2,12 +2,12 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { map, take } from 'rxjs';
 import { TagInfo } from '../models/tag.model';
-import { Firestore, collection, collectionData, orderBy, query } from '@angular/fire/firestore';
+import { LedgerService } from '../services/ledger.service';
 
 export const TagListGroupResolver: ResolveFn<TagInfo[][]> =
   () => {
-    const firestore = inject(Firestore)
-    return collectionData(query(collection(firestore, 'tagList'), orderBy('sort', 'asc')), { idField: 'id' })
+    const ledgerService = inject(LedgerService)
+    return ledgerService.getTagList()
       .pipe(take(1), map(tagList => {
         const result = (tagList as TagInfo[]).reduce((acc: TagInfo[][], cur, i) => {
           const num = Math.floor(i / 9)
