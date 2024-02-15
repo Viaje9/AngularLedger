ab26c722a228b723af2e74e1ce61e6837a3e1356
Brian <h99y5246cv@gmail.com>
2024-02-15 14:49:29 +0800

    新增統計圖表功能

--- src/app/features/setting/setting.routes.ts
+++ src/app/features/setting/setting.routes.ts
@@ -4,6 +4,7 @@ import { TagsManageComponent } from './tags-manage/tags-manage.component';
 import { AddTagComponent } from './add-tag/add-tag.component';
 import { AddTagInitDataResolver } from './add-tag/add-tag-init-data.resolver';
 import { BudgetComponent } from './budget/budget.component';
+import { StatisticsChartsComponent } from './statistics-charts/statistics-charts.component';
 
 export const SETTING_ROUTES: Routes = [
   {
@@ -27,6 +28,10 @@ export const SETTING_ROUTES: Routes = [
       {
         path: 'budget',
         component: BudgetComponent,
+      },
+      {
+        path: 'statisticsCharts',
+        component: StatisticsChartsComponent,
       },
     ],
   },
