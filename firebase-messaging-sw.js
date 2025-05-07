2797af01eb764cf094314b6b448bd9b10e0c7c17
Viaje9 <57859781+Viaje9@users.noreply.github.com>
2025-05-07 15:37:33 +0000

    Deploying to gh-pages from @ Viaje9/AngularLedger@0b390f4792da0305bbf4f87e0cf1afef7261ccb8 🚀

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,9 +5,9 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js",
 );
 
-importScripts("/firebase-config.js");
-
-const configJson = atob(self.FIREBASE_CONFIG);
+const configJson = atob(
+  "ewogICJwcm9qZWN0SWQiOiAiYW5ndWxhci1sZWRnZXIiLAogICJhcHBJZCI6ICIxOjYwMDg5NzA0MTYwNjp3ZWI6NWRiMWViYTNiNjk2ZmM4YzE5MTNjNyIsCiAgInN0b3JhZ2VCdWNrZXQiOiAiYW5ndWxhci1sZWRnZXIuYXBwc3BvdC5jb20iLAogICJhcGlLZXkiOiAiQUl6YVN5QS0zZnNNS3p4VVgzU1gtQTRzcmhYTkRDbWpKZGFxZ053IiwKICAiYXV0aERvbWFpbiI6ICJhbmd1bGFyLWxlZGdlci5maXJlYmFzZWFwcC5jb20iLAogICJtZXNzYWdpbmdTZW5kZXJJZCI6ICI2MDA4OTcwNDE2MDYiCn0K",
+);
 firebase.initializeApp(JSON.parse(configJson));
 
 const messaging = firebase.messaging();
